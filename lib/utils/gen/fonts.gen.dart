d77ebbb138a6d73331ecfe4c0a943f9e57939eec
Shakle <shakle@outlook.com>
2025-04-28 23:47:21 +0200

    prepare for app merge

---
+++ lib/utils/gen/fonts.gen.dart
@@ -0,0 +1,18 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: BasfIcons
+  static const String basfIcons = 'BasfIcons';
+
+  /// Font family: Roboto
+  static const String roboto = 'Roboto';
+}
