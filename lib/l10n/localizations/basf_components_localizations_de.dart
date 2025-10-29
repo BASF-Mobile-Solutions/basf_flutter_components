645efbda2bf675c1222945662e497dc09ceb87b9
Shakle <shakle@outlook.com>
2025-10-29 10:39:37 +0100

    add basf translations

--- lib/l10n/localizations/basf_components_localizations_de.dart
+++ lib/l10n/localizations/basf_components_localizations_de.dart
@@ -33,4 +33,25 @@ class BasfComponentsLocalizationsDe extends BasfComponentsLocalizations {
 /// The translations for German (`de_rlp`).
 class BasfComponentsLocalizationsDeRlp extends BasfComponentsLocalizationsDe {
   BasfComponentsLocalizationsDeRlp() : super('de_rlp');
+
+  @override
+  String get cameraNotAvailable => 'Kamera ist nicht verfügbar';
+
+  @override
+  String get codeScanSuccessPhrase => 'Code erfolgreich gescannt';
+
+  @override
+  String get provideCameraPermission => 'Kameraberechtigung erteilen';
+
+  @override
+  String get rescan => 'Erneut scannen';
+
+  @override
+  String get warning => 'Warnung';
+
+  @override
+  String get generalAbort => 'Abbrechen';
+
+  @override
+  String get generalConfirm => 'Bestätigen';
 }
--- lib/l10n/localizations/basf_components_localizations_zh.dart
+++ lib/l10n/localizations/basf_components_localizations_zh.dart
@@ -9,33 +9,75 @@ class BasfComponentsLocalizationsZh extends BasfComponentsLocalizations {
   BasfComponentsLocalizationsZh([String locale = 'zh']) : super(locale);
 
   @override
-  String get cameraNotAvailable => 'Camera is not available';
+  String get cameraNotAvailable => '相机不可用';
 
   @override
-  String get codeScanSuccessPhrase => 'Code scanned successfully';
+  String get codeScanSuccessPhrase => '代码扫描成功';
 
   @override
-  String get provideCameraPermission => 'Provide Camera Permission';
+  String get provideCameraPermission => '授予相机权限';
 
   @override
-  String get rescan => 'Rescan';
+  String get rescan => '重新扫描';
 
   @override
-  String get warning => 'Warning';
+  String get warning => '警告';
 
   @override
-  String get generalAbort => 'Cancel';
+  String get generalAbort => '中止';
 
   @override
-  String get generalConfirm => 'Confirm';
+  String get generalConfirm => '确认';
 }
 
 /// The translations for Chinese, using the Han script (`zh_Hans`).
 class BasfComponentsLocalizationsZhHans extends BasfComponentsLocalizationsZh {
   BasfComponentsLocalizationsZhHans() : super('zh_Hans');
+
+  @override
+  String get cameraNotAvailable => '相机不可用';
+
+  @override
+  String get codeScanSuccessPhrase => '代码扫描成功';
+
+  @override
+  String get provideCameraPermission => '授予相机权限';
+
+  @override
+  String get rescan => '重新扫描';
+
+  @override
+  String get warning => '警告';
+
+  @override
+  String get generalAbort => '中止';
+
+  @override
+  String get generalConfirm => '确认';
 }
 
 /// The translations for Chinese, using the Han script (`zh_Hant`).
 class BasfComponentsLocalizationsZhHant extends BasfComponentsLocalizationsZh {
   BasfComponentsLocalizationsZhHant() : super('zh_Hant');
+
+  @override
+  String get cameraNotAvailable => '相機不可用';
+
+  @override
+  String get codeScanSuccessPhrase => '代碼掃描成功';
+
+  @override
+  String get provideCameraPermission => '授予相機權限';
+
+  @override
+  String get rescan => '重新掃描';
+
+  @override
+  String get warning => '警告';
+
+  @override
+  String get generalAbort => '中止';
+
+  @override
+  String get generalConfirm => '確認';
 }
